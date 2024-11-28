6042c0bfd2bfb1c91fc619299f31c9a8f97262c3
Hennadii <bzpg545256@gmail.com>
2024-11-28 18:25:02 +0000

    debug and secret key

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q$#=ri3cp0$x+1@$2%tb!v6%t1n&uxe(_x(m@u+b+9$!!1fw#m'
-
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-q$#=ri3cp0$x+1@$2%tb!v6%t1n&uxe(_x(m@u+b+9$!!1fw#m")
+#SECRET_KEY = 'django-insecure-q$#=ri3cp0$x+1@$2%tb!v6%t1n&uxe(_x(m@u+b+9$!!1fw#m'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
